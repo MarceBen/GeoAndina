fb7f7e5a30a5762b2bc1a69dcb7305cc4e284079
MarceBen <marcelobeni2007@gmail.com>
2026-08-02 01:42:37 -0500

    refactor workflow and split geodetic/utm interface

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ model.load_model()
 def login():
 
     if session.get("Logged") == True:
-        return redirect(url_for("index"))
+        return redirect(url_for("main_menu"))
 
     if request.method == "GET":
         return render_template("login.html")
@@ -36,16 +36,37 @@ def login():
 
         if password == ACCESS_PASSWORD:
             session["Logged"] = True
-            return redirect(url_for("index"))
+            return redirect(url_for("main_menu"))
         
         else:
             return render_template("login.html", error = "Contraseña Incorrecta")
 
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/main_menu", methods=["GET", "POST"])
+def main_menu():
 
-def index():
+    if session.get("Logged") != True:
+        return redirect(url_for("login"))
+
+    if request.method == "GET":
+        return render_template("main_menu.html")
+
+    coordinate_type = request.form.get("CoordinateType")
+
+    if coordinate_type == "Geodetic":
+        return redirect(url_for("geodetic"))
+
+    elif coordinate_type == "UTM":
+        return redirect(url_for("utm_zone"))
+
+    return render_template("main_menu.html")
+
+
+
+@app.route("/geodetic", methods=["GET", "POST"])
+
+def geodetic():
 
     if session.get("Logged") != True:
         return redirect(url_for("login"))
@@ -56,23 +77,8 @@ def index():
 
     if request.method == "POST":
 
-
-        coordinate_type = request.form.get("CoordinateType")
-
-        if coordinate_type not in ["Geodetic", "UTM"]:
-            raise ValueError("El tipo de coordenada debe ser Geodética o UTM")
-
-        if coordinate_type == "UTM":
-
-            utm_zone = int(request.form.get("UTMZone", 18 ))
-
-            if utm_zone not in [17, 18, 19]:
-                raise ValueError("La zona UTM debe ser 17, 18 o 19")
-
-        
-
-
         try:
+
             quantity = int(request.form.get("quantity", 1))
             action = request.form.get("action")
 
@@ -82,7 +88,8 @@ def index():
 
 
             if action  == "generate":
-                return render_template("index.html", quantity=quantity)
+
+                return render_template("geodetic.html", quantity=quantity)
 
             elif action == "calculate":
 
@@ -92,52 +99,43 @@ def index():
                     
                     calculation_type = request.form.get(f"CalculationType_{i}")
                     coordinate_format = request.form.get(f"CoordinateFormat_{i}")
-            
-                    if coordinate_type == "UTM":
-            
-                        east = float(request.form.get(f"East_{i}", 0 ))
-                        north = float(request.form.get(f"North_{i}", 0 ))
-                        latitude, longitude = utm_to_geodetic(east, north, utm_zone)
-            
-                    elif coordinate_type == "Geodetic":
-            
-                        if coordinate_format == "DD":
-            
-                            latitude = float(request.form.get(f"Latitude_{i}", 0 ))
-                            longitude = float(request.form.get(f"Longitude_{i}", 0 ))
-            
-                        elif coordinate_format == "DM":
-            
-                            latitude_degree = float(request.form.get(f"LatitudeDegree_{i}", 0 ))
-                            latitude_minutes = float(request.form.get(f"LatitudeMinutes_{i}", 0 ))
-            
-                            latitude = dm_to_decimal(latitude_degree, latitude_minutes)
-            
-                            longitude_degree = float(request.form.get(f"LongitudeDegree_{i}", 0 ))
-                            longitude_minutes = float(request.form.get(f"LongitudeMinutes_{i}", 0 ))
-            
-                            longitude = dm_to_decimal(longitude_degree, longitude_minutes)
-            
-            
-                        elif coordinate_format == "DMS":
-            
-                            latitude_degree = float(request.form.get(f"LatitudeDegreeDMS_{i}", 0 ))
-                            latitude_minutes = float(request.form.get(f"LatitudeMinutesDMS_{i}", 0 ))
-                            latitude_seconds = float(request.form.get(f"LatitudeSecondsDMS_{i}", 0 ))
-            
-                            latitude = dms_to_decimal(latitude_degree, latitude_minutes, latitude_seconds)
-            
-                            longitude_degree = float(request.form.get(f"LongitudeDegreeDMS_{i}", 0 ))
-                            longitude_minutes = float(request.form.get(f"LongitudeMinutesDMS_{i}", 0 ))
-                            longitude_seconds = float(request.form.get(f"LongitudeSecondsDMS_{i}", 0 ))
-            
-                            longitude = dms_to_decimal(longitude_degree, longitude_minutes, longitude_seconds)
-            
-                        else:
-                            raise ValueError("Formato de coordenada no válido")
 
+                    if coordinate_format == "DD":
+            
+                        latitude = float(request.form.get(f"Latitude_{i}", 0 ))
+                        longitude = float(request.form.get(f"Longitude_{i}", 0 ))
+            
+                    elif coordinate_format == "DM":
+            
+                        latitude_degree = float(request.form.get(f"LatitudeDegree_{i}", 0 ))
+                        latitude_minutes = float(request.form.get(f"LatitudeMinutes_{i}", 0 ))
+            
+                        latitude = dm_to_decimal(latitude_degree, latitude_minutes)
+            
+                        longitude_degree = float(request.form.get(f"LongitudeDegree_{i}", 0 ))
+                        longitude_minutes = float(request.form.get(f"LongitudeMinutes_{i}", 0 ))
+            
+                        longitude = dm_to_decimal(longitude_degree, longitude_minutes)
+            
+            
+                    elif coordinate_format == "DMS":
+            
+                        latitude_degree = float(request.form.get(f"LatitudeDegreeDMS_{i}", 0 ))
+                        latitude_minutes = float(request.form.get(f"LatitudeMinutesDMS_{i}", 0 ))
+                        latitude_seconds = float(request.form.get(f"LatitudeSecondsDMS_{i}", 0 ))
+            
+                        latitude = dms_to_decimal(latitude_degree, latitude_minutes, latitude_seconds)
+            
+                        longitude_degree = float(request.form.get(f"LongitudeDegreeDMS_{i}", 0 ))
+                        longitude_minutes = float(request.form.get(f"LongitudeMinutesDMS_{i}", 0 ))
+                        longitude_seconds = float(request.form.get(f"LongitudeSecondsDMS_{i}", 0 ))
+            
+                        longitude = dms_to_decimal(longitude_degree, longitude_minutes, longitude_seconds)
+            
                     else:
-                        raise ValueError("Tipo de coordenada no válido")
+                        raise ValueError("Formato de coordenada no válido")
+
+               
 
                     if calculation_type == "OrthometricHeight":
                                     
@@ -157,29 +155,125 @@ def index():
                     
 
                     results.append ({
-                        "CalculationType": calculation_type,
-                        "latitude": latitude,
-                        "longitude": longitude,
-                        "orthometric_height": float(orthometric_height), 
-                        "ellipsoidal_height": float(ellipsoidal_height)
-                        })
+                            "CalculationType": calculation_type,
+                            "latitude": latitude,
+                            "longitude": longitude,
+                            "orthometric_height": float(orthometric_height), 
+                            "ellipsoidal_height": float(ellipsoidal_height)
+                            })
 
                                     
-                return render_template("index.html", quantity = quantity, results=results) 
+                return render_template("geodetic.html", quantity = quantity, results=results) 
             
             else:
                 raise ValueError("Accion Invalida")   
 
         except ValueError as e:
-            return render_template("index.html", quantity = quantity, results = results, error = str(e))
+            return render_template("geodetic.html", quantity = quantity, results = results, error = str(e))
 
         except Exception as e:
-            return render_template("index.html", quantity = quantity, results = results, error = "Ocurrio un error inesperado")
-            
-            
+            return render_template("geodetic.html", quantity = quantity, results = results, error = "Ocurrio un error inesperado")
 
-    return render_template("index.html", quantity = quantity, results = results)
+    return render_template("geodetic.html", quantity = quantity, results = results)
 
+@app.route("/utm_zone", methods=["GET", "POST"])
+def utm_zone():
 
+    if session.get("Logged") != True:
+        return redirect(url_for("login"))
+
+    if request.method == "GET":
+        return render_template("utm_zone.html")
+
+    if request.method == "POST":
+
+        utm_zone = request.form.get("UTMZone")
+
+        if utm_zone not in ["17", "18", "19"]:
+            return render_template("utm_zone.html", error="Seleccione una zona válida.")
+
+        session["UTMZone"] = int(utm_zone)
+
+        return redirect(url_for("utm"))
+
+@app.route("/utm", methods=["GET", "POST"])
+
+def utm():
+
+    if session.get("Logged") != True:
+        return redirect(url_for("login"))
+
+    results = None
+    quantity = None
+
+    if request.method == "POST":
+         
+        try:
+        
+            quantity = int(request.form.get("quantity", 1))
+            action = request.form.get("action")
+        
+            if quantity > MAX_QUANTITY or quantity < MIN_QUANTITY:
+                raise ValueError("La cantidad debe estar entre 1 y 1000")
+        
+            if action  == "generate":
+        
+                return render_template("utm.html", quantity=quantity)
+        
+            elif action == "calculate":
+        
+                        results = []
+                    
+                        for i in range (quantity):
+                            
+                            calculation_type = request.form.get(f"CalculationType_{i}")
+
+                            east = float(request.form.get(f"East_{i}", 0))
+                            north = float(request.form.get(f"North_{i}", 0))
+
+                            utm_zone = session.get("UTMZone")
+
+                            latitude, longitude = utm_to_geodetic(east, north, utm_zone)
+                            
+        
+                            if calculation_type == "OrthometricHeight":
+                                            
+                                ellipsoidal_height = float(request.form.get(f"EllipsoidalHeight_{i}", 0 ))
+                                            
+                                orthometric_height = calculate_orthometric_height(model, latitude, longitude, ellipsoidal_height)
+                                            
+                            elif calculation_type == "EllipsoidalHeight":
+                                            
+                                orthometric_height = float(request.form.get(f"OrthometricHeight_{i}", 0 ))
+                                                
+                                ellipsoidal_height = calculate_ellipsoidal_height(model, latitude, longitude, orthometric_height)
+                                            
+                            else:
+                                raise ValueError("Tipo de cálculo no válido")
+        
+                            
+        
+                            results.append ({
+                                    "CalculationType": calculation_type,
+                                    "latitude": latitude,
+                                    "longitude": longitude,
+                                    "orthometric_height": float(orthometric_height), 
+                                    "ellipsoidal_height": float(ellipsoidal_height)
+                                    })
+        
+                                            
+                        return render_template("utm.html", quantity = quantity, results=results) 
+                    
+            else:
+                raise ValueError("Accion Invalida")   
+        
+        except ValueError as e:
+                    return render_template("utm.html", quantity = quantity, results = results, error = str(e))
+        
+        except Exception as e:
+            return render_template("utm.html", quantity = quantity, results = results, error = "Ocurrio un error inesperado")
+        
+    return render_template("utm.html", quantity = quantity, results = results)
+        
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -2,111 +2,80 @@ const inputCard = document.querySelectorAll(".coordinate-card");
 
 function updateCard(inputCard)
 {
-    const coordinateType = inputCard.querySelector(".coordinate-type");
     const geodeticFormat = inputCard.querySelector(".geodetic-format");
     const calculationType = inputCard.querySelector(".calculation-type");
-    const utmFields = inputCard.querySelector(".utm-fields");
 
     const ddFields = inputCard.querySelector(".dd-fields");
     const dmFields = inputCard.querySelector(".dm-fields");
     const dmsFields = inputCard.querySelector(".dms-fields");
 
-    const utmZoneFields = inputCard.querySelector(".utmzone-fields");
-    const geodeticFormatCmbox = inputCard.querySelector(".geodeticformat-fields");
-
-
     const ellipsoidalFields = inputCard.querySelector(".ellipsoidal-fields");
     const orthometricFields = inputCard.querySelector(".orthometric-fields");
 
-
-
-    if(coordinateType.value === "Geodetic")
+    // Formato geodésico (solo existe en geodetic.html)
+    if (geodeticFormat)
     {
-        if(geodeticFormat.value === "DD" )
+        if (geodeticFormat.value === "DD")
         {
             ddFields.style.display = "block";
             dmFields.style.display = "none";
             dmsFields.style.display = "none";
-            utmFields.style.display = "none";
-            utmZoneFields.style.display = "none";
-            geodeticFormatCmbox.style.display = "block";
         }
         else if (geodeticFormat.value === "DM")
         {
             ddFields.style.display = "none";
             dmFields.style.display = "block";
             dmsFields.style.display = "none";
-            utmFields.style.display = "none";
-            utmZoneFields.style.display = "none";
-            geodeticFormatCmbox.style.display = "block";
         }
         else if (geodeticFormat.value === "DMS")
         {
             ddFields.style.display = "none";
             dmFields.style.display = "none";
             dmsFields.style.display = "block";
-            utmFields.style.display = "none";
-            utmZoneFields.style.display = "none";
-            geodeticFormatCmbox.style.display = "block";
-
         }
         else
         {
             throw new Error("Formato geodesico invalido.");
         }
     }
-    else if (coordinateType.value === "UTM")
-    {
-        utmFields.style.display = "block";
-        geodeticFormatCmbox.style.display = "none";
-        ddFields.style.display = "none";
-        dmFields.style.display = "none";
-        dmsFields.style.display = "none";
-        utmZoneFields.style.display = "block";
-    }
-    else
-    {
-        throw new Error("Tipo de coordenada invalido.");
-    }
 
-    if(calculationType.value === "OrthometricHeight")
+    // Tipo de cálculo (existe tanto en geodetic.html como en utm.html)
+    if (calculationType)
     {
-        orthometricFields.style.display = "none";
-        ellipsoidalFields.style.display = "block";
+        if (calculationType.value === "OrthometricHeight")
+        {
+            orthometricFields.style.display = "none";
+            ellipsoidalFields.style.display = "block";
+        }
+        else if (calculationType.value === "EllipsoidalHeight")
+        {
+            orthometricFields.style.display = "block";
+            ellipsoidalFields.style.display = "none";
+        }
+        else
+        {
+            throw new Error("Tipo de cálculo invalido.");
+        }
     }
-    else if (calculationType.value === "EllipsoidalHeight")
-    {
-        orthometricFields.style.display = "block";
-        ellipsoidalFields.style.display = "none";
-    }
-    else
-    {
-        throw new Error("Tipo de cálculo invalido.");
-    }
-
-    
-        
-    
 
 }
 
 
 inputCard.forEach(card => {
 
-    const coordinateType = card.querySelector(".coordinate-type");
     const geodeticFormat = card.querySelector(".geodetic-format");
     const calculationType = card.querySelector(".calculation-type");
-    const utmZoneFields = card.querySelector(".utmzone-fields");
-    const geodeticFormatCmbox = card.querySelector(".geodeticformat-fields");
-    
 
     updateCard(card);
 
-    coordinateType.addEventListener("change", () => updateCard(card));
-    geodeticFormat.addEventListener("change", () => updateCard(card));
-    calculationType.addEventListener("change", () => updateCard(card));
+    if (geodeticFormat)
+    {
+        geodeticFormat.addEventListener("change", () => updateCard(card));
+    }
 
-
+    if (calculationType)
+    {
+        calculationType.addEventListener("change", () => updateCard(card));
+    }
 
 });
-
